339726635aabd7d583cc9c5a57d30bf35f026d77
Francois Aguet <francois@broadinstitute.org>
2021-03-26 16:09:43 +0000

    Added colorbar option

--- qtl/plot.py
+++ qtl/plot.py
@@ -46,7 +46,8 @@ def get_axgrid(nr, nc, ntot=None, sharex=False, sharey=False,
                x_offset=6, y_offset=6,
                dl=0.5, aw=2, dx=0.75, dr=0.25,
                db=0.5, ah=2, dy=0.75, dt=0.25,
-               colorbar=False, tri=None):
+               colorbar=False, ds=0.15, cw=0.15, ct=0, ch=None,
+               tri=None, fontsize=10, hide=['top', 'right']):
     """
     """
     if ntot is None:
@@ -69,10 +70,19 @@ def get_axgrid(nr, nc, ntot=None, sharex=False, sharey=False,
                 ax = fig.add_axes([(dl+i*(aw+dx))/fw, (db+(nr-j-1)*(ah+dy))/fh, aw/fw, ah/fh],
                                   sharex=axes[0] if sharex and n>0 else None,
                                   sharey=axes[0] if sharey and n>0 else None)
-                format_plot(ax, fontsize=10, x_offset=x_offset, y_offset=y_offset)
+                format_plot(ax, fontsize=fontsize, hide=hide, x_offset=x_offset, y_offset=y_offset)
                 axes.append(ax)
                 n += 1
-    return axes
+
+    if not colorbar:
+        return axes
+    else:
+        if ch is None:
+            ch = ah/2
+        cax = fig.add_axes([(dl+nc*aw+(nc-1)*dx+ds)/fw, (db+nr*ah+(nr-1)*dy-ch-ct)/fh, cw/fw, ch/fh])
+        # cax = fig.add_axes([(dl+aw+ds)/fw, (db+ah-ch-ct)/fh, cw/fw, ch/fh])
+        return axes, cax
+
 
 
 def format_plot(ax, tick_direction='out', tick_length=4, hide=['top', 'right'],
